8c7fe58e751e7c7ddb18183c7cfbd8d48cc686d7
Vito <vitohartanto@mail.ugm.ac.id>
2024-12-06 15:58:11 +0700

    UserRepository

--- src/Domains/users/entities/RegisterUser.js
+++ src/Domains/users/entities/RegisterUser.js
@@ -1,27 +1,35 @@
-class RegisteredUser {
+class RegisterUser {
   constructor(payload) {
     this._verifyPayload(payload);
 
-    const { id, username, fullname } = payload;
+    const { username, password, fullname } = payload;
 
-    this.id = id;
     this.username = username;
+    this.password = password;
     this.fullname = fullname;
   }
 
-  _verifyPayload({ id, username, fullname }) {
-    if (!id || !username || !fullname) {
-      throw new Error('REGISTERED_USER.NOT_CONTAIN_NEEDED_PROPERTY');
+  _verifyPayload({ username, password, fullname }) {
+    if (!username || !password || !fullname) {
+      throw new Error('REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY');
     }
 
     if (
-      typeof id !== 'string' ||
       typeof username !== 'string' ||
+      typeof password !== 'string' ||
       typeof fullname !== 'string'
     ) {
-      throw new Error('REGISTERED_USER.NOT_MEET_DATA_TYPE_SPECIFICATION');
+      throw new Error('REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+
+    if (username.length > 50) {
+      throw new Error('REGISTER_USER.USERNAME_LIMIT_CHAR');
+    }
+
+    if (!username.match(/^[\w]+$/)) {
+      throw new Error('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER');
     }
   }
 }
 
-module.exports = RegisteredUser;
+module.exports = RegisterUser;
--- src/Domains/users/entities/_test/RegisterUser.test.js
+++ src/Domains/users/entities/_test/RegisterUser.test.js
@@ -1,47 +1,67 @@
-const RegisteredUser = require('../RegisteredUser');
+const RegisterUser = require('../RegisterUser');
 
-describe('a RegisteredUser entities', () => {
+describe('a RegisterUser entities', () => {
   it('should throw error when payload did not contain needed property', () => {
     // Arrange
     const payload = {
-      username: 'dicoding',
-      fullname: 'Dicoding Indonesia',
+      username: 'abc',
+      password: 'abc',
     };
 
     // Action and Assert
-    expect(() => new RegisteredUser(payload)).toThrowError(
-      'REGISTERED_USER.NOT_CONTAIN_NEEDED_PROPERTY'
+    expect(() => new RegisterUser(payload)).toThrowError(
+      'REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY'
     );
   });
 
   it('should throw error when payload did not meet data type specification', () => {
     // Arrange
     const payload = {
-      id: 123,
-      username: 'dicoding',
-      fullname: 'Dicoding Indonesia',
+      username: 123,
+      fullname: true,
+      password: 'abc',
     };
-
     // Action and Assert
-    expect(() => new RegisteredUser(payload)).toThrowError(
-      'REGISTERED_USER.NOT_MEET_DATA_TYPE_SPECIFICATION'
+    expect(() => new RegisterUser(payload)).toThrowError(
+      'REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION'
     );
   });
-
-  it('should create registeredUser object correctly', () => {
+  it('should throw error when username contains more than 50 character', () => {
     // Arrange
     const payload = {
-      id: 'user-123',
+      username: 'dicodingindonesiadicodingindonesiadicodingindonesiadicoding',
+      fullname: 'Dicoding Indonesia',
+      password: 'abc',
+    };
+    // Action and Assert
+    expect(() => new RegisterUser(payload)).toThrowError(
+      'REGISTER_USER.USERNAME_LIMIT_CHAR'
+    );
+  });
+  it('should throw error when username contains restricted character', () => {
+    // Arrange
+    const payload = {
+      username: 'dico ding',
+      fullname: 'dicoding',
+      password: 'abc',
+    };
+    // Action and Assert
+    expect(() => new RegisterUser(payload)).toThrowError(
+      'REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER'
+    );
+  });
+  it('should create registerUser object correctly', () => {
+    // Arrange
+    const payload = {
       username: 'dicoding',
       fullname: 'Dicoding Indonesia',
+      password: 'abc',
     };
-
     // Action
-    const registeredUser = new RegisteredUser(payload);
-
+    const { username, fullname, password } = new RegisterUser(payload);
     // Assert
-    expect(registeredUser.id).toEqual(payload.id);
-    expect(registeredUser.username).toEqual(payload.username);
-    expect(registeredUser.fullname).toEqual(payload.fullname);
+    expect(username).toEqual(payload.username);
+    expect(fullname).toEqual(payload.fullname);
+    expect(password).toEqual(payload.password);
   });
 });
